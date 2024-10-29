bedc4f3b07959eaec7bfadb4997e316061fbb591
Fabian Åman <Fabianaman03@gmail.com>
2024-10-29 13:31:51 +0100

    Crated decka handeling

---
+++ Deck.h
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int randomCard();
+
+struct cardInfo // Storing card Value
+{
+    int cardValue;              // The value to use when calc
+    std::string cardInfomation; // String to display card
+};
+class Deck
+{
+public:
+    // All cards in deck
+    std::vector<std::string> color = {"♥", "♦", "♠", "♣"};
+    std::vector<int> value = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+};
+cardInfo DrawCard()
+{
+    Deck deck;
+    srand(time(0));
+    int cardDrawnValue = deck.value.at(0 + rand() % 12); // Drawn
+    std::string cardInfo = deck.color.at(0 + rand() % 3) + " ";
+    switch (cardDrawnValue)
+    {
+    case 1:
+        cardInfo += "Ace";
+        break;
+    case 11:
+        cardInfo += "Queen";
+        break;
+    case 12:
+        cardInfo += "King";
+        break;
+    default:
+        cardInfo += std::to_string(cardDrawnValue);
+        break;
+    }
+    if (cardDrawnValue > 10)
+    {
+        cardDrawnValue = 10;
+    }
+    return {cardDrawnValue, cardInfo};
+}
+int main()
+{
+    Deck card;
+    cardInfo drawnCard = DrawCard();
+    std::cout << drawnCard.cardValue << " " << drawnCard.cardInfomation;
+}
