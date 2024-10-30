9530633b2f7ffa5d551befea227b1367713255a2
John Collinder <90156021+flaxdrop@users.noreply.github.com>
2024-10-30 09:41:56 +0100

    matat in playername.c++ filen i gameSetup.h

--- Product_MVP/gameSetup.h
+++ Product_MVP/gameSetup.h
@@ -5,5 +5,15 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
+
+std::string getPlayerName(){
+std::string name;
+std::cout << "Please enter your name: ";
+std::getline(std::cin, name);
+
+    return name;
+}
+
 
 #endif
