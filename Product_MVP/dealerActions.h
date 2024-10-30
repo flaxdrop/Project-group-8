fb0026b5cd42add54ac21811dcfd16228220393d
John Collinder <90156021+flaxdrop@users.noreply.github.com>
2024-10-30 10:20:36 +0100

    Update dealerActions.h with dealersturn function

--- Product_MVP/dealerActions.h
+++ Product_MVP/dealerActions.h
@@ -6,4 +6,31 @@
 #include <string>
 #include <vector>
 
+void dealerTurn(std::vector<Card> &dealerHand, std::vector<Card> &deck, int &deckIndex)
+{
+    std::cout << "Dealer's turn:\n";
+
+    while (true)
+    {
+        int handValue = calculateHandValue(dealerHand);
+
+        if (handValue >= 17)
+        {
+            break;
+        }
+
+        dealerHand.push_back(deck[deckIndex]);
+        deckIndex++;
+
+        std::cout << "Dealer draws a card: " << dealerHand.back().value << " (Total: " << handValue << ")\n";
+        Sleep(2000);
+    }
+
+    std::cout << "Dealer has a total of " << calculateHandValue(dealerHand) << ".\n";
+}
+
+
+
+
+
 #endif
